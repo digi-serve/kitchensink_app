c0804427d65471261b6667473d5993ac8296a131
nate <7259@pm.me>
2021-12-10 16:45:42 +0700

    Add carousel test

--- integration.js
+++ integration.js
@@ -22,13 +22,33 @@ before(() => {
 
 beforeEach(() => {
   Common.AuthLogin(cy);
-  // Common.RunSQL(cy, folderName, ["reset_tables.sql"]);
+  cy.visit("/").wait(1500);
+  Common.RunSQL(cy, folderName, ["reset_tables.sql"]);
 });
 
-it("Smoke Test", () => {
-  describe("App Loads", () => {
-    cy.visit("/").wait(1500);
+describe("Smoke Test", () => {
+  it("App Loads", () => {
     cy.get('[data-cy="portal_work_menu_sidebar"]').click();
     cy.get('[data-cy="0ac51d6c-7c95-461c-aa8b-7da00afc4f48"]').should("exist");
+  });
+});
+
+describe.only("Widget Tests", () => {
+  beforeEach(() => {
+    cy.get('[data-cy="portal_work_menu_sidebar"]').click();
+    cy.get('[data-cy="0ac51d6c-7c95-461c-aa8b-7da00afc4f48"]').click();
+    cy.get('[data-cy="cb77ced0-a803-46b7-8a79-f9084d75d51c"]').click();
+  });
+  describe("Carousel", () => {
+    it("Exists", () => {
+      Common.RunSQL(cy, folderName, ["add_testkcs.sql"]);
+      cy.get(
+        '[data-cy="carousel 54827db6-497b-43ae-96f9-153b63a9c977"]'
+      ).should("exist");
+      // Select tab
+      // cy.get(
+      //   "tab-Carousel-e56d4ad0-d879-43c0-934a-a4004fcc7579-b52e6e96-5033-4c7f-a104-29bd5ddcac4a"
+      // ).click();
+    });
   });
 });
