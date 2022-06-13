09bf8d00288a80670a6c036d941898e95b57562c
Achoobert <greenace14@gmail.com>
2022-06-13 17:07:09 +0700

    remove non-existant link

--- integration.js
+++ integration.js
@@ -21,7 +21,7 @@ const WidgetTestCases = [
 ];
 
 const ProcessTestCases = [
-   require("./test_cases/process_test-kcs.js"),
+   // require("./test_cases/process_test-kcs.js"),
    require("./test_cases/process_test-kcs-onCreate-process.js"),
 ];
 
