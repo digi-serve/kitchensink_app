64441922a83fea178c1dbb5a77be86e873632ff1
Achoobert <greenace14@gmail.com>
2022-06-23 16:33:53 +0700

    [add] Multilingual default text on a child record
    Test: insert default text to a child record-> when generated by parent record's add
    https://github.com/digi-serve/kitchensink_app/issues/33

--- test_cases/process_test-kcs-onCreate-process.js
+++ test_cases/process_test-kcs-onCreate-process.js
@@ -8,8 +8,7 @@ export default (folderName, Common) => {
             '[data-cy="tab onCreate bc6a8b04-709c-46ce-a273-cb3550a17282 1169d7cf-d03d-4bd5-b282-4897b3329d7c"]'
          ).click();
       });
-      it('SIMPLIFIED Process inserts"', () => {
-         // TODO remove after fixing the next test!
+      it('Process inserts a record after inserting a label value on the tab "process > onCreate"', () => {
          cy.get(
             '[data-cy="string label 06a93149-590d-4e4f-9463-5ff43a689fd1 2172ba78-b327-42a1-8918-d97852234aee"]'
          ).type("test label");
@@ -20,32 +19,21 @@ export default (folderName, Common) => {
 
          cy.get(".webix_progress_icon").should("not.exist");
 
+         // is Default Text - multilingual label - for child record working?
+         cy.log(
+            "we are attempting to set this in both the object and the process. Neither are working, it seems to be because the column allows for multilingual data."
+         );
+         cy.log(
+            "There are two labels: the one we are looking for contains the word 'None'"
+         );
          cy.get(
-            '[data-cy="ABViewGrid_4c2af349-da19-407e-9db0-ab34d1a35837_datatable"'
-         ).should("contain", "test label");
-         cy.get(
-            '[data-cy="ABViewGrid_4c2af349-da19-407e-9db0-ab34d1a35837_datatable"'
-         ).should("contain", "Manual Text");
-         cy.get(
-            '[data-cy="ABViewGrid_4c2af349-da19-407e-9db0-ab34d1a35837_datatable"'
-         ).should("contain", "updated");
-      });
-      it.skip('Process inserts a record after inserting a label value on the tab "process > onCreate"', () => {
-         cy.get(
-            '[data-cy="string label 06a93149-590d-4e4f-9463-5ff43a689fd1 2172ba78-b327-42a1-8918-d97852234aee"]'
-         ).type("test label");
+            '[data-cy="ABViewGrid_4c2af349-da19-407e-9db0-ab34d1a35837_datatable"]'
+         ).contains("None");
 
-         cy.get(
-            '[data-cy="button save 2172ba78-b327-42a1-8918-d97852234aee"]'
-         ).click();
-
-         cy.get(".webix_progress_icon").should("not.exist");
-
-         // is text in child record working
          cy.get(
             '[data-cy="ABViewGrid_4c2af349-da19-407e-9db0-ab34d1a35837_datatable"]  > .webix_ss_body > .webix_ss_center'
          )
-            .find(".webix_column")
+            .find(".webix_column ")
             .should(($data) => {
                expect($data, "5 columns").to.have.length(5);
                expect($data.eq(0), "label").to.contain("test label");
