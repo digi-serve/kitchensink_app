c454e7f6aadbc39afe0206d2aa8f08a05fa82ae7
James Duncan <james@digiserve.org>
2023-03-16 10:51:27 +0700

    the sql statements are run already in the beforeEach that begins at line #47

--- integration.cy.js
+++ integration.cy.js
@@ -71,11 +71,11 @@ describe("Smoke Test", () => {
 
 describe("Widget Tests", () => {
    beforeEach(() => {
-      Common.RunSQL(cy, folderName, [
-         "add_testkcs.sql",
-         "add_testkcs2-Menu.sql",
-         "add_testkcs2-ScopedData.sql",
-      ]);
+      // Common.RunSQL(cy, folderName, [
+      //    "add_testkcs.sql",
+      //    "add_testkcs2-Menu.sql",
+      //    "add_testkcs2-ScopedData.sql",
+      // ]);
       cy.visit("/");
       cy.get('[data-cy="portal_work_menu_sidebar"]')
          .should("be.visible")
