647c03c2c7f3813118d4e2f29f0e128613771ff0
wongpraatan <wongpratan@hotmail.com>
2023-04-17 23:53:59 +0700

    *Update how to pull label in the detail widget

--- test_cases/widget_detail.js
+++ test_cases/widget_detail.js
@@ -6,10 +6,12 @@ export default () => {
          ).click();
 
          cy.get(
+            '[view_id="ABViewDetailText_5db49524-c1f8-4a82-81ee-4022ba8f60a2_detailItemLabel"]'
+         ).should("include.text", "test-kcs-id");
+
+         cy.get(
             '[data-cy="detail text testkcsid c4f98caa-71c1-4c36-9587-f8db050d2e2f aa1c0a4d-001c-4227-970b-4cf1c676ad20"]'
-         )
-            .should("include.text", "test-kcs-id")
-            .and("include.text", "Test-KCS-");
+         ).should("include.text", "Test-KCS-");
       });
    });
 };
