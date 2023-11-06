9db9ac078b9444bf21c47ce173a0ef9a5cfbdb65
Nate <7259@pm.me>
2023-11-06 11:05:24 +0700

    refactor as a single test

--- test_cases/widget_carousel.js
+++ test_cases/widget_carousel.js
@@ -1,77 +1,32 @@
-export default (folderName, Common) => {
-   describe.only("Carousel", () => {
-      it("Exists", () => {
-         Common.RunSQL(cy, folderName, ["add_testkcs.sql"]);
-         cy.get(
-            '[data-cy="carousel 54827db6-497b-43ae-96f9-153b63a9c977"]'
-         ).should("exist");
-      });
-
-      //1. can find the label the text "Default image"
+export default () => {
+   describe("Carousel", () => {
       it("Check Carousel Component", () => {
-         cy.get(
-            '[data-cy="carousel 54827db6-497b-43ae-96f9-153b63a9c977"]'
-         ).then((data) => {
-            expect(data.text(), "Label").to.include("Default image");
-         });
-      });
-
-      //2. can find the button which has the icon "fa fa-arrows-alt"
-      it("Check fa arrows", () => {
+         //1. can find the label the text "Default image"
          cy.get('[data-cy="carousel 54827db6-497b-43ae-96f9-153b63a9c977"]')
-            .scrollIntoView()
-            // not finding span
-            .find("span", { timeout: 50000, retryInterval: 500 })
-            .should(($span) => {
-               expect($span).to.have.length(2);
-
-               const className = $span[0].className;
-
-               expect(className).to.match(/fa fa-arrows-alt/);
+            .as("carousel")
+            .should("exist")
+            .then((data) => {
+               expect(data.text(), "Label").to.include("Default image");
             });
+         //2. can find the button which has the icon "fa fa-arrows-alt"
+         cy.get("@carousel")
+            .find("span")
+            .should("have.length", 2)
+            .first()
+            .should("have.class", "fa")
+            .and("have.class", "fa-arrows-alt");
+         //3. can find the button "previous"
+         cy.get("@carousel")
+            .find(".webix_nav_button_prev")
+            .and("have.class", "webix_nav_button_side");
+         //4. can find the button "next"
+         cy.get("@carousel")
+            .find(".webix_nav_button_next")
+            .and("have.class", "webix_nav_button_side");
+         //5. can find the "nav panel"
+         cy.get("@carousel")
+            .find(".webix_nav_panel")
+            .should("have.class", "webix_nav_panel_side");
       });
-      //3. can find the button "previous"
-      it("Check Previous btn", () => {
-         cy.get(
-            '[data-cy="carousel 54827db6-497b-43ae-96f9-153b63a9c977"]'
-         ).find("div");
-         cy.get("div")
-            .should("have.class", "webix_nav_button_side webix_nav_button_prev")
-            .and("have.class", "webix_nav_button_prev");
-      }); //End 3
-      //4. can find the button "next"
-      it("check next btn", () => {
-         cy.get(
-            '[data-cy="carousel 54827db6-497b-43ae-96f9-153b63a9c977"]'
-         ).find("div");
-         cy.get("div")
-            .should("have.class", "webix_nav_button_side webix_nav_button_next")
-            .and("have.class", "webix_nav_button_next");
-      }); //End 4
-      //5. can find the "nav panel"
-      it("check nav panel", () => {
-         cy.get(
-            '[data-cy="carousel 54827db6-497b-43ae-96f9-153b63a9c977"]'
-         ).find("div");
-         cy.get("div")
-            .should("have.class", "webix_nav_panel webix_nav_panel_side")
-            .and("have.class", "webix_nav_panel");
-      }); //End 5
-
-      //6. the first "nav item" status should "active"
-
-      it("first nav active", () => {
-         cy.get(
-            '[data-cy="carousel 54827db6-497b-43ae-96f9-153b63a9c977"]'
-         ).find("div");
-         cy.get("div")
-
-            .should(
-               "have.class",
-               "webix_nav_panel webix_nav_panel_side",
-               "active"
-            )
-            .first();
-      }); //End 6
-   }); //End describe
-}; //end Export
+   });
+};
