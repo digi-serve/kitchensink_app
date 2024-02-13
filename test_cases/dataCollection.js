2f9989d56604939c719510e4d157a26352e77572
wongpraatan <wongpratan@hotmail.com>
2024-02-13 20:48:11 +0700

    +Add a test case to cover keep cursor when DC got load from paging - https://github.com/digi-serve/appbuilder_class_core/pull/210#discussion_r1480907500

--- test_cases/dataCollection.js
+++ test_cases/dataCollection.js
@@ -20,12 +20,12 @@ export default () => {
             .as("list")
             .should("be.visible")
             .find(".webix_dataview_item")
-            .should("have.length", 2);
+            .should("have.length", 18);
          // add a new record, should see it
          cy.request("POST", `/app_builder/model/${testkcsObjectID}`, {
             singlelinetext: "new",
          });
-         cy.get("@list").find(".webix_dataview_item").should("have.length", 3);
+         cy.get("@list").find(".webix_dataview_item").should("have.length", 18);
       });
 
       it("Linked DC loads data according to parent cursor", () => {
@@ -104,6 +104,63 @@ export default () => {
             .should("have.length", 1)
             .first()
             .should("contain", "Record A");
+      });
+
+      it("Should keep selected cursor when cursor of a linked DC changes", () => {
+         // Select the Tabview
+         cy.get(
+            '[data-cy="tab Cursor 3f06ceac-ac30-4e64-b165-e30002fff60c b82e7941-b47f-477d-9c10-1d7ef85185ff"]',
+         )
+            .should("be.visible")
+            .click();
+
+         // Select a linked cursor
+         cy.get('[data-cy="ABViewGrid_497b6d7d-c4e2-40a4-8bcb-fcaa24cf875f_datatable"]')
+            .should("be.visible")
+            .find(".webix_ss_body .webix_ss_right .webix_cell")
+            .first()
+            .click();
+
+         // Click to show the scroll bar
+         cy.get('[data-cy="ABViewGrid_d08be402-470e-4b9d-b5f4-72e27426522c_datatable"]')
+            .find(".webix_ss_body .webix_ss_right")
+            .click();
+
+         // Close the popup
+         cy.get('[data-cy="Popup Close Button Edit TestKCS 5403c1cf-57f9-463d-b306-799d3641be11"]')
+            .click();
+
+         // Drag the scroll bar to bottom
+         cy.get('[data-cy="ABViewGrid_d08be402-470e-4b9d-b5f4-72e27426522c_datatable"]')
+            .find(".webix_c_scroll_y")
+            .should("be.visible")
+            .trigger('mousedown')
+            .trigger('mousemove', { which: 1, pageY: 2000 })
+            .trigger('mouseup');
+
+         // Select the edit item
+         cy.get('[data-cy="ABViewGrid_d08be402-470e-4b9d-b5f4-72e27426522c_datatable"]')
+            .find(".webix_ss_body .webix_ss_right")
+            .find('[aria-rowindex="30"]')
+            .click();
+
+         // Enter new data
+         const newData = "NEW ONE";
+         cy.get('input[data-cy="string singlelinetext 78d60c46-5523-474d-8e0c-d8d6db814be8 5acd8c09-a0d2-4d3b-a993-b3c251858ed6"]')
+            .should("be.visible")
+            .clear()
+            .type(newData);
+
+         // Save
+         cy.get('[data-cy="button save 5acd8c09-a0d2-4d3b-a993-b3c251858ed6"]')
+            .click();
+
+         // Assert
+         cy.get('[data-cy="ABViewGrid_d08be402-470e-4b9d-b5f4-72e27426522c_datatable"]')
+            .find(".webix_ss_body .webix_ss_center")
+            .find('[aria-rowindex="30"]')
+            .should("contain", newData)
+            .should("have.class", "webix_row_select");
       });
    });
 };
