51cb38b6f8bb52d06ee17f3192739822f80b6c4e
Johnny <johnny@digiserve.org>
2024-05-20 04:26:35 -0500

    [add] additional check to ensure Form initialization is successful

--- test_cases/widget_form_save.js
+++ test_cases/widget_form_save.js
@@ -20,9 +20,9 @@ export default () => {
             .should("be.visible")
             .type("Test Record Rules", { delay: 0 });
 
-         cy.get(
-            '[data-cy="button save 7e074587-ddc5-4d34-9f37-a0ab88a4258b"]',
-         ).click({ force: true });
+         cy.get('[data-cy="button save 7e074587-ddc5-4d34-9f37-a0ab88a4258b"]')
+            .should("be.enabled")
+            .click({ force: true });
          cy.get(
             '[data-cy^="ABViewGrid_42938e52-9da9-4ece-b492-deb253244d3e_datatable"]',
          ).scrollIntoView();
